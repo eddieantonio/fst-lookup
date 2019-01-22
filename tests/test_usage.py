b160ae98fc9f5cc6e8622b6e7c2ef58247b8e585
Eddie Antonio Santos <easantos@ualberta.ca>
2019-01-22 16:32:26 -0700

    Test that an FST can be loaded from a file and used to do a lookup.

--- tests/test_usage.py
+++ tests/test_usage.py
@@ -4,5 +4,13 @@
 from fst_lookup import FST
 
 
-def test_load_from_file(datadir):
-    fst = FST.from_file(datadir / 'eat.att.gz')
+def test_load_from_file(shared_datadir):
+    """
+    Integration test for loading the FST from a file.
+    """
+    fst = FST.from_file(shared_datadir / 'eat.att.gz')
+    # Do a transduction that outputs multiple results.
+    assert set(fst.lookup('eats')) == {
+            ('eat', '+N', '+Mass'),
+            ('eat', '+V', '+3P', '+Sg'),
+    }
