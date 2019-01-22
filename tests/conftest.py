cc3547a6b158feb49bdda1743bce5658e98e09b9
Eddie Antonio Santos <easantos@ualberta.ca>
2019-01-22 13:21:17 -0700

    Extract eat_fst_txt to a shared fixture.

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import gzip
+from pathlib import Path
+
+import pytest
+
+
+@pytest.fixture
+def eat_fst_txt(shared_datadir: Path) -> str:
+    """
+    Return the text content of the "eat" FST.
+
+        foma[0]: read lexc eat.lexc
+        1007 bytes. 20 states, 24 arcs, 6 paths.
+        foma[1]: sigma
+        Sigma: +3P +Mass +N +Past +PastPart +PresPart +Sg +V a e g i n s t
+        Size: 15.
+
+    """
+    with gzip.open(str(shared_datadir / 'eat.att.gz'), 'rt') as text_file:
+        return text_file.read()  # type: ignore
--- tests/test_parse.py
+++ tests/test_parse.py
@@ -5,10 +5,6 @@
 Tests for parsing the AT&T text format.
 """
 
-import gzip
-
-import pytest
-
 from fst_lookup import parse_text
 
 
@@ -27,19 +23,3 @@ def test_parse_simple(eat_fst_txt: str):
     assert len(result.states) == 20
     assert len(result.arcs) == 24
     assert result.accepting_states == {19}
-
-
-@pytest.fixture
-def eat_fst_txt(shared_datadir) -> str:
-    """
-    Return the text content of the "eat" FST.
-
-        foma[0]: read lexc eat.lexc
-        1007 bytes. 20 states, 24 arcs, 6 paths.
-        foma[1]: sigma
-        Sigma: +3P +Mass +N +Past +PastPart +PresPart +Sg +V a e g i n s t
-        Size: 15.
-
-    """
-    with gzip.open(str(shared_datadir / 'eat.att.gz'), 'rt') as text_file:
-        return text_file.read()  # type: ignore
