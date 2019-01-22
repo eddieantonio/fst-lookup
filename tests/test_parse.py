e4c93f8a90639a5bc603c5474f56b1879d93e0aa
Eddie Antonio Santos <easantos@ualberta.ca>
2019-01-22 12:33:21 -0700

    Factor out reading 'eat' FST to a fixture.

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -7,19 +7,16 @@ Tests for parsing the AT&T text format.
 
 import gzip
 
+import pytest
+
 from fst_lookup import parse_text
 
 
-def test_parse_simple(shared_datadir):
-    # foma[0]: read lexc eat.lexc
-    # 1007 bytes. 20 states, 24 arcs, 6 paths.
-    # foma[1]: sigma
-    # Sigma: +3P +Mass +N +Past +PastPart +PresPart +Sg +V a e g i n s t
-    # Size: 15.
-    with gzip.open(str(shared_datadir / 'eat.att.gz'), 'rt') as text_file:
-        text = text_file.read()
-
-    result = parse_text(text)
+def test_parse_simple(eat_fst_txt: str):
+    """
+    Parse a simple FST WITHOUT flag diacritics.
+    """
+    result = parse_text(eat_fst_txt)
     assert len(result.sigma) == 15
     multichar_symbols = set('+3P +Mass +N +Past +PastPart '
                             '+PresPart +Sg +V'.split())
@@ -29,3 +26,19 @@ def test_parse_simple(shared_datadir):
     assert set(result.graphemes.values()) == graphemes
     assert len(result.states) == 20
     assert len(result.arcs) == 24
+
+
+@pytest.fixture
+def eat_fst_txt(shared_datadir) -> str:
+    """
+    Return the text content of the "eat" FST.
+
+        foma[0]: read lexc eat.lexc
+        1007 bytes. 20 states, 24 arcs, 6 paths.
+        foma[1]: sigma
+        Sigma: +3P +Mass +N +Past +PastPart +PresPart +Sg +V a e g i n s t
+        Size: 15.
+
+    """
+    with gzip.open(str(shared_datadir / 'eat.att.gz'), 'rt') as text_file:
+        return text_file.read()  # type: ignore
