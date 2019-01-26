2bc2a28099b7a05ac10590e19431a2414970d4d8
Eddie Antonio Santos <easantos@ualberta.ca>
2019-01-25 20:02:32 -0800

    Test lookdown.

--- tests/test_apply.py
+++ tests/test_apply.py
@@ -24,3 +24,20 @@ def test_apply_up_eat_fst(surface_form: str, analyses: set, eat_fst_txt: str):
     """
     fst = FST.from_text(eat_fst_txt)
     assert set(fst.lookup(surface_form)) == analyses
+
+
+@pytest.mark.parametrize('analysis,surface_form', [
+    (('eat' '+V'), 'eat'),
+    (('eat' '+V' '+Past'), 'ate'),
+    (('eat' '+V' '+PresPart'), 'eating'),
+    (('eat' '+V' '+PastPart'), 'eaten'),
+    (('eat' '+V' '+3P' '+Sg'), 'eats'),
+    (('eat' '+N' '+Mass'), 'eats'),
+])
+def test_lookdown_eat_fst(analysis: str, surface_form: set, eat_fst_txt: str):
+    """
+    Apply up (lookup) on an FST **WITHOUT** flag diacritics.
+    """
+    fst = FST.from_text(eat_fst_txt)
+    actual, = fst.lookdown(analysis)
+    assert actual == surface_form
