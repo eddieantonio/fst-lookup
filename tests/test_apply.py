9640633fc38be8c2edd909e3c5a9af7edebac5b3
Eddie Antonio Santos <easantos@ualberta.ca>
2019-01-29 10:25:29 -0700

    Test analyze with flag diacritics.

--- tests/test_apply.py
+++ tests/test_apply.py
@@ -20,7 +20,7 @@ from fst_lookup import FST
 ])
 def test_analyze_eat_fst(surface_form: str, analyses: set, eat_fst_txt: str):
     """
-    Apply up (lookup) on an FST **WITHOUT** flag diacritics.
+    Apply up (analyze) on an FST **WITHOUT** flag diacritics.
     """
     fst = FST.from_text(eat_fst_txt)
     assert set(fst.analyze(surface_form)) == analyses
@@ -41,3 +41,29 @@ def test_generate_eat_fst(analysis: str, surface_form: set, eat_fst_txt: str):
     fst = FST.from_text(eat_fst_txt)
     actual, = fst.generate(analysis)
     assert actual == surface_form
+
+
+@pytest.mark.parametrize('surface_form,analysis', [
+    ('drinkables', ('drinkable', '+Adj', '+Pl')),
+    ('drinkable', ('drinkable', '+Adj')),
+    ('drink', ('drink', '+V', '+Inf')),
+    ('payables', ('payable', '+Adj', '+Pl')),
+    ('payable', ('payable', '+Adj')),
+    ('pay', ('pay', '+V', '+Inf')),
+    ('doables', ('doable', '+Adj', '+Pl')),
+    ('doable', ('doable', '+Adj')),
+    ('do', ('do', '+V', '+Inf')),
+    ('undrinkables', ('UN+', 'drinkable', '+Adj' '+Pl')),
+    ('undrinkable', ('UN+', 'drinkable', '+Adj')),
+    ('unpayables', ('UN+', 'payable', '+Adj' '+Pl')),
+    ('unpayable', ('UN+', 'payable', '+Adj')),
+    ('undoables', ('UN+', 'doable', '+Adj' '+Pl')),
+    ('undoable', ('UN+', 'doable', '+Adj')),
+    ('undo', ('UN+', 'do', '+V' '+Inf')),
+])
+def test_analyze_flag_fst(surface_form: str, analysis, english_flags_fst_txt: str):
+    """
+    Apply up (lookup) on an FST **WITH** simple flag diacritics.
+    """
+    fst = FST.from_text(english_flags_fst_txt)
+    assert set(fst.analyze(surface_form)) == {analysis}
