5cc43d228c1fd84e3e5b42509cc071e4e8b54f7c
Eddie Antonio Santos <easantos@ualberta.ca>
2019-07-17 09:45:39 -0600

    Test usage of new surface_form attribute.

--- tests/test_usage.py
+++ tests/test_usage.py
@@ -16,3 +16,24 @@ def test_load_from_file(shared_datadir):
     }
     # Transduce the other way!
     assert set(fst.generate('eat' '+V' '+Past')) == {'ate'}
+
+
+def test_load_from_file_flipped(shared_datadir):
+    """
+    Integration test loading an FST from a file where its
+    UPPER side is the surface form, and its
+    LOWER side is the deep form
+    (following HFST conventions rather and XFST conventions).
+    """
+    fst = FST.from_file(shared_datadir / 'tae.fomabin', labels='flip')
+
+    # The following tests are INTENTIONALLY the same as for
+    # test_load_from_file(). However, the FST is different than in that test.
+
+    # Do a transduction that outputs multiple results.
+    assert set(fst.analyze('eats')) == {
+            ('eat', '+N', '+Mass'),
+            ('eat', '+V', '+3P', '+Sg'),
+    }
+    # Transduce the other way!
+    assert set(fst.generate('eat' '+V' '+Past')) == {'ate'}
