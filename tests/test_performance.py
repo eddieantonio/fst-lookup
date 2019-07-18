234b477abda34a77b8f58dc6cf811c83b793ea11
matt <syan4@ualberta.ca>
2019-07-18 11:41:15 -0600

    polish

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -7,7 +7,7 @@ def test_hfstol_analysis_performance(cree_hfstol_analyzer: FST, cree_foma_analyz
     """
     Test hfstol analysis has at least 100 times the performance when there are 800 words
     """
-    data = ["niskak"]*800
+    data = ["niskak"] * 800
     time0 = time.time()
     for _ in list(cree_hfstol_analyzer.analyze_in_bulk(data)):
         list(_)
@@ -19,16 +19,16 @@ def test_hfstol_analysis_performance(cree_hfstol_analyzer: FST, cree_foma_analyz
         list(_)
     time3 = time.time()
 
-    a = 100*(time1-time0)
-    b = time3-time2
-    print(a, b)
+    a = 100 * (time1 - time0)
+    b = time3 - time2
     assert a < b
+
 
 def test_hfstol_generation_performance(cree_hfstol_generator: FST, cree_foma_generator: FST):
     """
     Test hfstol generation has at least 100 times the performance when there are 150 words
     """
-    data = [''.join(('nîskâw', '+V', '+II', '+Cnj', '+Prs', '+3Sg'))]*150
+    data = [''.join(('nîskâw', '+V', '+II', '+Cnj', '+Prs', '+3Sg'))] * 150
     time0 = time.time()
     for _ in list(cree_hfstol_generator.generate_in_bulk(data)):
         list(_)
@@ -40,7 +40,6 @@ def test_hfstol_generation_performance(cree_hfstol_generator: FST, cree_foma_gen
         list(_)
     time3 = time.time()
 
-    a = 100*(time1-time0)
-    b = time3-time2
-    print(a, b)
+    a = 100 * (time1 - time0)
+    b = time3 - time2
     assert a < b
