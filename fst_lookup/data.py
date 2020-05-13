ec20ff1ee8f2fc3d67e873e3d9655436669ee8ac
Eddie Antonio Santos <easantos@ualberta.ca>
2020-05-13 15:47:32 -0600

    refactor: move StateID to typedefs [new module]

--- fst_lookup/data.py
+++ fst_lookup/data.py
@@ -14,11 +14,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Callable, NamedTuple, NewType
+from typing import NamedTuple
 
 from .symbol import Symbol
-
-StateID = NewType("StateID", int)
+from .typedefs import StateID
 
 
 class Arc(
---
+++ fst_lookup/typedefs.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+from typing import NewType
+
+StateID = NewType("StateID", int)
