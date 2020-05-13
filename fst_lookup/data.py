0604151adf4b1c1d2a902f9c179fde02453d3945
Eddie Antonio Santos <easantos@ualberta.ca>
2020-05-13 16:02:03 -0600

    feat: use c-arc when importable

--- fst_lookup/data.py
+++ fst_lookup/data.py
@@ -18,5 +18,9 @@
 Classes that define data in the FST.
 """
 
-from .fallback_data import Arc
 from .typedefs import StateID
+
+try:
+    from ._fst_lookup import Arc
+except ImportError:
+    from .fallback_data import Arc  # type: ignore
