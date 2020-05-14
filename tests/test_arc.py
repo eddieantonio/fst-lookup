99d59af5f274b04fb289675d567e2a11d068592f
Eddie Antonio Santos <easantos@ualberta.ca>
2020-05-13 19:07:23 -0600

    test: various Arc.__hash__() tests

--- tests/test_arc.py
+++ tests/test_arc.py
@@ -5,7 +5,7 @@
 Tests for the Arc class
 """
 
-from hypothesis import given
+from hypothesis import assume, given
 from hypothesis.strategies import characters, integers
 
 from fst_lookup.data import Arc, StateID
@@ -23,6 +23,7 @@ def test_eq_trivial(state, upper, lower, dest):
     b = Arc(s0, upper, lower, s1)
 
     assert a == b
+    assert hash(a) == hash(b)
 
 
 @given(integers(min_value=0), characters(), characters(), integers(min_value=0))
@@ -34,3 +35,35 @@ def test_eq_different_symbols(state, upper, lower, dest):
     b = Arc(s0, Grapheme(upper), Grapheme(lower), s1)
 
     assert a == b
+    assert hash(a) == hash(b)
+
+
+MAX_DISTINCT_HASH = 2 ** 16 - 1
+
+
+@given(
+    integers(min_value=0, max_value=MAX_DISTINCT_HASH),
+    integers(min_value=1, max_value=MAX_DISTINCT_HASH),
+    characters(),
+    characters(),
+)
+def test_hash(initial: int, state_incr: int, upper: str, lower: str):
+    """
+    The hash function has a property that, while there are less than 2**16
+    states, each arc will bucket seperately depending on its starting state.
+    This helps distribute the states a bit.
+
+    Outside of this safe range, the hash function may collide :/
+    """
+    s0 = StateID(initial)
+    s1 = StateID(initial + state_incr)
+    assert s0 < s1
+    assume(s1 <= MAX_DISTINCT_HASH)
+
+    c1 = Grapheme(upper)
+    c2 = Grapheme(lower)
+
+    a = Arc(s0, c1, c2, s0)
+    b = Arc(s1, c1, c2, s0)
+
+    assert hash(a) < hash(b)
