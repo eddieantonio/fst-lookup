3980f885bc8574a5f66d102a587ecd0e9e7a1372
Eddie Antonio Santos <easantos@ualberta.ca>
2020-05-14 04:09:40 -0600

    build: enable C warnings

--- build.py
+++ build.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
+import sysconfig
 from distutils.command.build_ext import build_ext  # type: ignore
 from distutils.core import Extension
 
-extensions = [Extension("fst_lookup._fst_lookup", sources=["fst_lookup/_fst_lookup.c"])]
+# Get compiler flags from the current Python version:
+extra_compile_args = sysconfig.get_config_vars("CFLAGS")
+extra_compile_args += ["-std=c99", "-Wall", "-Wextra"]
+
+extensions = [
+    Extension(
+        "fst_lookup._fst_lookup",
+        sources=["fst_lookup/_fst_lookup.c"],
+        extra_compile_args=extra_compile_args,
+    )
+]
 
 
 def build(setup_kwargs):
